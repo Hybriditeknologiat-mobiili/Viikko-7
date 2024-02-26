55fa35b5767704fbb3bf2ec7d3dcd2874d6de546
LeeviSeppala <t2sele00@students.oamk.fi>
2024-02-26 18:29:59 +0200

    1.0
    ei ihan nyt ruvennut toimimaan mutta aika loppuu kesken selvitystyöhön

--- theme/App.js
+++ theme/App.js
@@ -1,31 +1,35 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import MainAppbar from './components/MainAppBar';
 import Settings from './screens/Settings';
-import ThemeProvider from './context/useTheme';
+import Home from './screens/Home'; // Import Home component
+import { ThemeProvider } from './context/useTheme'; // Import ThemeProvider
 
 export default function App() {
-  const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
 
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StatusBar backgroundColor="#666" barStyle="light-content" />
-        <Stack.Navigator
-          initialRouteName='Home'
-          screenOptions={{
-            header: (props) =>
-              <MainAppbar {...props} backgroundColor="#666" icon="cog" color="#fff" />
-          }}
-        >
-
-          <Stack.Screen name="Home">
-            {() =>
-              <Home />
-            }
-          </Stack.Screen>
-          <Stack.Screen name="Settings" component={Settings} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+    <ThemeProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <StatusBar backgroundColor="#666" barStyle="light-content" />
+          <Stack.Navigator
+            initialRouteName='Home'
+            screenOptions={{
+              header: (props) =>
+                <MainAppbar {...props} backgroundColor="#666" icon="cog" color="#fff" />
+            }}
+          >
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Settings" component={Settings} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </ThemeProvider>
   );
 }
 
